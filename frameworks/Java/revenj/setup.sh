8824987cd723d292bff26816e818d5b04749ba47
Rikard Pavelic <rikard@ngs.hr>
2015-12-22 13:46:53 +1100

    Replaced Servlet DSL with Revenj.
    Added DB tests.
    Both Java and C# version.

---
+++ frameworks/Java/revenj/setup.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+fw_depends java8 resin maven mono
+
+echo "Cleaning up..."
+rm -rf $TROOT/tmp $TROOT/model $TROOT/revenj.java $TROOT/dsl-clc.jar
+
+echo "Download DSL compiler client"
+wget -O $TROOT/dsl-clc.jar https://github.com/ngs-doo/dsl-compiler-client/releases/download/1.5.0/dsl-clc.jar
+
+echo "Compiling the server model, and downloading dependencies..."
+java -jar $TROOT/dsl-clc.jar \
+	temp=$TROOT/tmp/ \
+	force \
+	dsl=$TROOT/src \
+	manual-json \
+	namespace=dsl \
+	revenj.java=$TROOT/model/gen-model.jar \
+	no-prompt \
+	download
+
+echo "Adding model to local Maven repository..."
+mvn deploy:deploy-file \
+	-Durl=file://model \
+	-Dfile=model/gen-model.jar \
+	-DgroupId=dsl \
+	-DartifactId=gen-model \
+	-Dpackaging=jar \
+	-Dversion=1.0
+
+echo "Changing the database"
+cat $TROOT/web.xml | sed 's/localhost/'$DBHOST'/g' > $TROOT/src/main/webapp/WEB-INF/web.xml
+	
+mvn clean compile war:war
+rm -rf $RESIN_HOME/webapps/*
+cp target/revenj.war $RESIN_HOME/webapps/
+resinctl start
---
+++ frameworks/Java/revenj/src/main/java/hello/QueriesServlet.java
@@ -0,0 +1,23 @@
+package hello;
+
+import com.dslplatform.json.JsonWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class QueriesServlet extends HttpServlet {
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse res) throws ServletException, IOException {
+		res.setContentType("application/json");
+		final int count = Utils.parseBoundParam(req);
+		final Context ctx = Utils.getContext();
+		final JsonWriter json = ctx.json;
+		ctx.loadWorlds(count);
+		json.serialize(ctx.worlds, count);
+		json.toStream(res.getOutputStream());
+	}
+}
